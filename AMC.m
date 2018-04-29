97454493d0e7b0e5b7750afb9388e9b75b446e43
arun <marunk94@gmail.com>
2018-04-29 22:01:29 +0530

    16qamdone

--- AMC.m
+++ AMC.m
@@ -1,4 +1,4 @@
-function [] = AMC(fc,d,ber=0.1,ttl_symbl=1)
+function [bt_errr_rt_qpsk,bt_errr_rt_sixteenqam] = AMC(fc,d,ber=0.1,ttl_symbl=1)
 
 pkg load signal;
 pkg load nan;
@@ -14,7 +14,7 @@ No = -174; % Noise power
 fs = 3*fc;  % sampling frequency 
 slot_time = 10^(-3);
 
-T = 10^(-3)/14; % symbol time
+T = 10^(3)/14; % symbol time
 bt_errr = 0 ; % to count the error symbols 
 ttl_symbl = ttl_symbl; % total symbols to be sent 
 
@@ -62,13 +62,13 @@ for k = 1:ttl_symbl
 
     % detection
 
-    if (rx_sym1_int > 0 )
+    if (rx_sym1_int >= 0 )
       fin_sym1 = A;
     elseif (rx_sym1_int < 0)
       fin_sym1 = -A;
     endif
 
-    if (rx_sym2_int > 0)
+    if (rx_sym2_int >= 0)
       fin_sym2 = A;
     elseif (rx_sym2_int < 0)
       fin_sym2 = -A;
@@ -86,11 +86,34 @@ for k = 1:ttl_symbl
 endfor
 
 bt_errr_rt_qpsk = bt_errr/(2*ttl_symbl)
-% bit error rate for 16QAM
-for k = 1:ttl_symbl
 
-    tx_bt = randi([1 4],1,1); %save the symbol choosed to compare at the receiver
-    tx_sym = cell2mat(symbols(tx_bt)); % transmitted symbol
+% bit error rate for 16QAM
+ber = [];
+for k = 1:ttl_symbl
+    e = 0;
+    tx_bt = dec2bin(randi([1 16],1,1),4); %save the symbol choosed to compare at the receiver
+
+    if (tx_bt(1:2) == '00')
+      tx_sym_re = A;
+    elseif (tx_bt(1:2) == '01') 
+      tx_sym_re = -A;
+    elseif (tx_bt(1:2) == '10') 
+      tx_sym_re = 2*A;
+    elseif (tx_bt(1:2) == '11') 
+      tx_sym_re = -2*A;
+    end
+
+    if (tx_bt(3:4) == '00')
+      tx_sym_im = A;
+    elseif (tx_bt(3:4) == '01') 
+      tx_sym_im = -A;
+    elseif (tx_bt(3:4) == '10') 
+      tx_sym_im = 2*A;
+    elseif (tx_bt(3:4) == '11') 
+      tx_sym_im = -2*A;
+    end
+
+    tx_sym = tx_sym_re*cos(2*pi*fc*t)-tx_sym_im*sin(2*pi*fc*t); % transmitted symbol
     h = hx(k)+j*hy(k);  %channel distribution
 
     %received symbol
@@ -110,27 +133,32 @@ for k = 1:ttl_symbl
 
     % detection
 
-    if (rx_sym1_int > 0 )
-      fin_sym1 = A;
-    elseif (rx_sym1_int < 0)
-      fin_sym1 = -A;
-    endif
+    % if (rx_sym1_int > 0 )
+    %   fin_sym1 = A;
+    % elseif (rx_sym1_int < 0)
+    %   fin_sym1 = -A;
+    % endif
 
-    if (rx_sym2_int > 0)
-      fin_sym2 = A;
-    elseif (rx_sym2_int < 0)
-      fin_sym2 = -A;
-    endif
+    % if (rx_sym2_int > 0)
+    %   fin_sym2 = A;
+    % elseif (rx_sym2_int < 0)
+    %   fin_sym2 = -A;
+    % endif
+    sym = [A,-A,2*A,-2*A];
+    bits_rx = ['00','01','10','11'];
+    for l=1:length(sym)
+      err(l) = (sym(l)-rx_sym1_int)^2;
+      [~,minerr] = min(err);
+      fin_sym1 = bits_rx(minerr);
 
-
-    if(fin_sym1 != symbl_cmprsn(tx_bt,1))
-      bt_errr = bt_errr+1;
-    endif
-
-    if (fin_sym2 != symbl_cmprsn(tx_bt,2))
-      bt_errr = bt_errr+1;
-    endif
-    
+      err1(l) = (sym(l)-rx_sym1_int)^2;
+      [~,minerr] = min(err1);
+      fin_sym2 = bits_rx(minerr);
+    endfor
+    rerr = tx_bt(1:2) == fin_sym1;
+    imerr = tx_bt(3:4) == fin_sym2;
+    e = (2-sum(rerr(:)))+(2-sum(imerr(:)));
+    ber(k) = e/4*ttl_symbl; 
 endfor
 
-bt_errr_rt_qpsk = bt_errr/(2*ttl_symbl)
+bt_errr_rt_sixteenqam =mean(ber)
