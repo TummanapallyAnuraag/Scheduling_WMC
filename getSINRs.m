d773cbfca731136afd4ed1faa627949bfa14b9d9
TummanapallyAnuraag <tummanapally.anuraag@gmail.com>
2018-04-30 05:02:17 +0530

    Modified SINRk calculation algorithm

--- getSINRs.m
+++ getSINRs.m
@@ -22,15 +22,18 @@ function [MS] = getSINRs(MS)
         % It is sure that for 1 TTI the sub channel is flat fading.
         % so the channel gain is same as h throughout the TTI duration.
 
-        hx = normrnd(0,1,7,1);
-        hy = normrnd(0,1,7,1);
+        hx = normrnd(0,1,7,RB_Count);
+        hy = normrnd(0,1,7,RB_Count);
         h = hx + i*hy;
-        numerator = ( abs(h(1))^2 )/( ( MS(i).d_refcell )^gamma );
+        % Each row corresponds to each cell (total 7)
+        gain = abs(h).^2;
+        % gain(1, :) => array of gain of all RBs in 1st cell(eUTRAN)
+        numerator = ( gain(1,:) )./( ( MS(i).d_refcell )^gamma );
         denominator = N0*RB_BW;
         for bs_index = 1:6
-            denominator = denominator + ( abs(h(1 + bs_index))^2 )/( ( MS(i).distance(bs_index) )^gamma );
+            denominator = denominator + ( gain(1+bs_index,:) )./( ( MS(i).distance(bs_index) )^gamma );
         end
-        MS(i).SINRk = numerator/denominator;
+        MS(i).SINRk = numerator./denominator;
 
         % SINR calculation => for all RBs taken together
         % for 1 TTI
@@ -43,6 +46,8 @@ function [MS] = getSINRs(MS)
         gain = sum(abs(h).^2, 2);
         % Sum along the row => gain is added within same cell(eUTRAN)
 
+        % To change the dimension and reset the values...
+        numerator = denominator = 0;
         numerator = ( gain(1) )/( ( MS(i).d_refcell )^gamma );
         for bs_index = 1:6
             denominator = denominator + ( gain(1+bs_index) )/( ( MS(i).distance(bs_index) )^gamma );
